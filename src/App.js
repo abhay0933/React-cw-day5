eb558f7cf21c42b58a2af8b9bf206934bc5a4e5b
abhay0933 <abhay80912@gmail.com>
2023-12-23 19:57:20 +0530

    day5 CW done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import {useState} from "react";
+
 function App() {
+  const [subject, setsubject] = useState("");
+  const [hour, sethours] = useState(0);
+  const [planner, setplanner]= useState([]);
+
+  const addthings = () =>{
+    setplanner([...planner,{subjectName: subject, Hours: hour}])
+  }
+
+  const changethings = (e, fun) =>{
+    fun(e.target.value);
+  }
+
+  const increasehour = (idx) => {
+      setplanner((prevplanner) => {
+        const newplanner = [...prevplanner];
+        newplanner[idx] = {
+          ...newplanner[idx],
+          Hours: parseInt(newplanner[idx].Hours)+1,
+        };
+        return newplanner;
+      });
+  }
+
+  const decreasehour = (idx) => {
+    setplanner((prevplanner) => {
+      const newplanner = [...prevplanner];
+      newplanner[idx] = {
+        ...newplanner[idx],
+        Hours: parseInt(newplanner[idx].Hours)-1,
+      };
+      return newplanner;
+    });
+}
+
   return (
+   
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="container">
+       <h1>Padhai Kar le Bhai Job Chaiye Toh</h1>
+       <input type="text" placeholder="Enter" className='subject' value ={subject} onChange={(e)=>changethings(e, setsubject)}/>
+       <input type="number" placeholder="hours" className='hour' value ={hour} onChange={(e)=>changethings(e, sethours)}/>
+       <button className="addbtn" onClick={addthings}>Add+</button>
+     </div>
+    
+      {
+        planner.map((ele, index)=>(
+          <div className='addedthings'>
+          {ele.subjectName} {ele.Hours+ "Hour"}
+          <button className='plus' onClick={() => increasehour(index)}>+</button>
+          <button className='minus' onClick={() => decreasehour(index)}>-</button>
+          
+          </div>
+        ))
+      }
+     
     </div>
   );
 }
